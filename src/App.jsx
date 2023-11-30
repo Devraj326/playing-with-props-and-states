fe41bed82f84ca352c2957f19bd5bbef0cef8a37
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2023-11-30 12:06:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import lake from "./image/lake.JPG";
 import GallaryFooter from './components/GallaryFooter';
-import GallaryHeader from './components/gallaryHeader';
+import GallaryHeader from './components/GallaryHeader';
 import GallaryBody from './components/GallaryBody';
 
 const imageData = [
